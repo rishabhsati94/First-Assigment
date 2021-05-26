cd36e4da8f1e051d18c7a12fee6a3c16191b4e5a
Rishabh Kumar Sati <rishabhsati94@gmail.com>
2021-05-27 02:02:12 +0530

    most recently visited site

--- BrowsingHistory/src/history/Browser.java
+++ BrowsingHistory/src/history/Browser.java
@@ -5,19 +5,33 @@ import java.util.Stack;
 public class Browser {
 	
 	private static Stack<String> history = new  Stack<String>();
+	
+//	Most Recent Check
+	public static String mostRecentlyVisitedSite() {
+		// TODO Auto-generated method stub
+		if(history.isEmpty()== true) {
+			return "History is Empty";
+		}
+		else {
+			return history.peek();
+		}
+
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-
+//		push history in a stack
+		
 		history.push("google.com");
 		history.push("gmail.com");
 		history.push("facebook.com");
 		history.push("youtube.com");
 		history.push("javatpoint.com");
 		history.push("stackoverflow.com");
+		
+		System.out.println("Most Recent call is: "+mostRecentlyVisitedSite());
 
-		System.out.println("History of Browser is: "+history);
 	}
 
 }
